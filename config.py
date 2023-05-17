b67faf7c2299b312e564187b4df7ae347c60938a
Aditya Vikram <aditya.vikram.iisc@gmail.com>
2023-05-17 20:11:21 +0530

    architechture of neumf writte

---
+++ config.py
@@ -0,0 +1,6 @@
+config_nuemf = {
+                "num_users" : 100,
+                "num_items" : 100,
+                "latent_vector_dim" : 75,
+                "mlp_layer_neurons" : [200, 150, 100]
+                }
