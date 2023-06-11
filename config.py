b464024e5ea123205876632906dcb83285bec4aa
Aditya Vikram <aditya.vikram.iisc@gmail.com>
2023-06-11 11:44:05 +0530

    config file updated

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+
+# num_users: take the max value + 1
+# num_items: take the max_value + 1
+
 config_ncf = {
-                "num_users" : 100,
-                "num_items" : 100,
+                "num_users" : 6041,
+                "num_items" : 3953,
                 "mlp_latent_vector_dim" : 32,
                 "gmf_latent_vector_dim": 32,
                 "mlp_layer_neurons" : [64, 32, 16]
